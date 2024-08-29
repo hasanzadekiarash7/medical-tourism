951e27cce42c7d9a6f434874d812783b8d2cebf3
Kiyarash Software Developer <hasanzadekiarash7@gmail.com>
2024-08-29 19:16:18 +0330

    install require and useful packages for project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt']
+  modules: [
+    '@pinia/nuxt',
+    'nuxt-aos',
+    '@nuxtjs/i18n',
+    '@nuxtjs/color-mode',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@formkit/auto-animate',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxtjs/seo',
+    'nuxt-lodash'
+  ]
 })
